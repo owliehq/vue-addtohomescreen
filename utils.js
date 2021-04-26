506e3f9a733724f50b5d885916494e6f8766c40f
Jordan Matejicek <Jordan@matejicek.me>
2021-04-26 16:50:49 +0200

    fixed dumb error

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 export const isStandalone = () => {
-  const webappsProtocols: ['file:', 'cordova:', 'capacitor:']
+  const webappsProtocols = ['file:', 'cordova:', 'capacitor:']
   return (window.matchMedia('(display-mode: standalone)').matches || (window.location && window.location.protocol && webappsProtocols.indexOf(window.location.protocol) !== -1))
 }
 /*
